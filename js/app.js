c5242b3543d991d1c3f77bb6e21eb3c5058febf9
Maxx Crawford <mcrawford@broco.com>
2017-05-17 22:32:20 -0500

    Created Sponsor card generator

--- js/app.js
+++ js/app.js
@@ -2,13 +2,23 @@ var canvas = document.getElementById("canvas")
 var downloadLnk = document.getElementById('download')
 
 var card_preview = new Vue({
-	el: '#card-wrapper',
+	el: '#speaker-wrapper',
 	data: {
 		image_url: 'http://placehold.it/270x400.jpg',
 		name: 'John Smith',
 		topic: 'Speaker Topic'
 	}
 })
+
+var card_preview = new Vue({
+	el: '#sponsor-wrapper',
+	data: {
+		image_url: 'http://placehold.it/400x400.jpg',
+		sponsor: 'Mozilla Dev Group',
+		level: '<title>'
+	}
+})
+
 
 function preview() {
 	console.log('preview');
